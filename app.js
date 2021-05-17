1dedb635e34601097ad076e5db8781e36a10191b
DhruvPrajapati123 <dhruvprajapati8200@gmail.com>
2021-05-17 14:58:48 +0530

    Level-4 Add hash function dcrypt for better security

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const express = require("express")
 const ejs = require("ejs")
 const bodyParser = require("body-parser")
 const mongoose = require("mongoose")
-var md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+// var md5 = require('md5');
 // const encrypt = require('mongoose-encryption');
 
 mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true}, {useUnifiedTopology: true})
@@ -41,31 +43,40 @@ app.get("/register", (req, res) => {
 
 app.post("/register", (req, res) => {
 
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+    
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("secrets")
+            }
+        })
+    });
 
-    newUser.save((err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("secrets")
-        }
-    })
+    
 })
 
 app.post("/login", (req, res) => {
+
     User.findOne({email: req.body.username}, (err, foundUser) => {
         if (err) {
             console.log(err);
         } else {
             if (foundUser) {
-                if (foundUser.password == md5(req.body.password)){
-                    res.render("secrets")
-                } else {
-                    res.send("Please Enter correct username and password")
-                }
+                // Load hash from your password DB.
+                bcrypt.compare(req.body.password, foundUser.password, function(err, result) {
+                    if (result == true){
+                        res.render("secrets")
+                    } else {
+                        res.send("Please Enter correct username and password")
+                    }
+                });
             }
         }
     })
